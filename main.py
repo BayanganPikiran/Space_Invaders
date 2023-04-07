2b4d8dba02e1cfb5c32e78cc417d8e456d799240
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-04-07 11:41:30 +0700

    added game_over()

--- main.py
+++ main.py
@@ -22,13 +22,6 @@ outerspace = pygame.image.load("outerspace.png")
 mixer.music.load("space_oddity.wav")
 mixer.music.play(-1)
 
-#Collision sounds
-# all_wrong = mixer.Sound("this_all_wrong.wav")
-# should_not_be_here = mixer.Sound("shouldnt_be_up_here.wav")
-# in_school = mixer.Sound("in_school.wav")
-# my_dreams = mixer.Sound("my_dreams.wav")
-how_dare = mixer.Sound("how_dare_you.wav")
-
 
 #Player
 player_spaceship = pygame.image.load("spaceship.png")
@@ -36,15 +29,10 @@ player_x_pos = 370
 player_y_pos = 500
 player_x_pos_change = 0
 
-
-
 #Create enemies
-jesus = pygame.image.load("jesus.png")
-monster = pygame.image.load("monster.png")
-trump = pygame.image.load("trump.png")
-gandhi = pygame.image.load("gandhi.png")
+
 greta = pygame.image.load("greta_thunberg.png")
-enemy_list = [jesus, monster, gandhi, trump, greta]
+enemy_list = []
 
 enemy_image = []
 enemy_x_pos = []
@@ -54,7 +42,7 @@ enemy_y_pos_change = []
 num_of_enemies = 6
 
 for i in range(num_of_enemies):
-    enemy_image.append(random.choice(enemy_list))
+    enemy_image.append(enemy_list)
     enemy_x_pos.append(random.randint(64, 735))
     enemy_y_pos.append(random.randint(50, 180))
     enemy_x_pos_change.append(2.5)
@@ -72,12 +60,18 @@ missile_state = "ready"
 score_value = 0
 font = pygame.font.Font("zai_CourierPolski1941.ttf", 46)
 
-score_pos_x = 290
+score_pos_x = 300
 score_pos_y = 10
+
+
 
 def show_score(x, y):
     score = font.render("Score: " + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
+
+def game_over():
+    g_over_text = font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(g_over_text, (280, 250))
 def create_player(x, y):
     screen.blit(player_spaceship, (x, y))
 
@@ -132,6 +126,13 @@ while running:
 
     #Enemy Movement
     for i in range(num_of_enemies):
+
+        #Game over
+        if enemy_y_pos[i] > 450:
+            for n in range(num_of_enemies):
+                enemy_y_pos[n] = 1000
+            game_over()
+
         enemy_x_pos[i] += enemy_x_pos_change[i]
         if enemy_x_pos[i] <= 0:
             enemy_x_pos_change[i] = 2.5
